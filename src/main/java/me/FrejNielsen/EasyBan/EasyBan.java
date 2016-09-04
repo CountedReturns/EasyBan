ff5a180d29aa48198f00365aef8512f8f930dbe4
Frej Alexander Nielsen <frejnielsen59@gmail.com>
2016-09-04 12:33:57 +0200

    Initial commit

---
+++ src/main/java/me/FrejNielsen/EasyBan/EasyBan.java
@@ -0,0 +1,15 @@
+package me.FrejNielsen.EasyBan;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EasyBan extends JavaPlugin {
+	
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+	
+}
